c8ac3dfb8d0dcad3fbd9cc9205f2298a3426d988
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-08-11 15:03:54 -0400

    ParticlesScript

--- PhotonEscape/Assets/Spawner.cs
+++ PhotonEscape/Assets/Spawner.cs
@@ -12,14 +12,11 @@ public class Spawner : MonoBehaviour {
 	public float SpawnLeastWait;
 	public int StartWait;
 	public bool stop;
-	int RandEnemy;
-//	SpriteRenderer render;
-		
+	int RandEnemy;		
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(waitSpawner());
-	//	render = gameObject.GetComponent(SpriteRenderer);
 	}
 	
 	// Update is called once per frame
@@ -32,7 +29,6 @@ public class Spawner : MonoBehaviour {
 		while (!stop) {
 			RandEnemy = Random.Range (0,4);
 			Vector3 SpawnPosition = new Vector3(0.9f,Random.Range(-SpawnValues.y,SpawnValues.y),0);
-			render.color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
 			Instantiate(Enemies[RandEnemy],SpawnPosition + transform.TransformPoint(0,0,0),gameObject.transform.rotation);
 			yield return new WaitForSeconds(SpawnWait);
 
